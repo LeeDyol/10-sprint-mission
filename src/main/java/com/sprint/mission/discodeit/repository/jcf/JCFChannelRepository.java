82357cf2f424dd107c42a8d3a0cd4dcef653b2ae
LeeDyol <dlekthf819203@naver.com>
2026-03-05 17:36:28 +0900

    refactor: JCF 및 File 기반 Repository 구현체 주석 처리

--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -1,53 +1,53 @@
-package com.sprint.mission.discodeit.repository.jcf;
-
-import com.sprint.mission.discodeit.entity.ChannelEntity;
-import com.sprint.mission.discodeit.repository.ChannelRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-@Repository
-@ConditionalOnProperty(
-        name = "discodeit.repository.type" ,
-        havingValue = "jcf" ,
-        matchIfMissing = true
-)
-public class JCFChannelRepository implements ChannelRepository {
-    private final List<ChannelEntity> data;
-
-    public JCFChannelRepository() {
-        data = new ArrayList<>();
-    }
-
-    // 채널 저장
-    @Override
-    public void save(ChannelEntity channel) {
-        data.removeIf(existChannel -> existChannel.getId().equals(channel.getId()));
-
-        data.add(channel);
-    }
-
-    // 채널 단건 조회
-    @Override
-    public Optional<ChannelEntity> findById(UUID channelId) {
-        return data.stream()
-                .filter(channel -> channel.getId().equals(channelId))
-                .findFirst();
-    }
-
-    // 채널 전체 조회
-    @Override
-    public List<ChannelEntity> findAll() {
-        return data;
-    }
-
-    // 채널 삭제
-    @Override
-    public void delete(ChannelEntity channel) {
-        data.remove(channel);
-    }
-}
+//package com.sprint.mission.discodeit.repository.jcf;
+//
+//import com.sprint.mission.discodeit.entity.ChannelEntity;
+//import com.sprint.mission.discodeit.repository.ChannelRepository;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//@Repository
+//@ConditionalOnProperty(
+//        name = "discodeit.repository.type" ,
+//        havingValue = "jcf" ,
+//        matchIfMissing = true
+//)
+//public class JCFChannelRepository implements ChannelRepository {
+//    private final List<ChannelEntity> data;
+//
+//    public JCFChannelRepository() {
+//        data = new ArrayList<>();
+//    }
+//
+//    // 채널 저장
+//    @Override
+//    public void save(ChannelEntity channel) {
+//        data.removeIf(existChannel -> existChannel.getId().equals(channel.getId()));
+//
+//        data.add(channel);
+//    }
+//
+//    // 채널 단건 조회
+//    @Override
+//    public Optional<ChannelEntity> findById(UUID channelId) {
+//        return data.stream()
+//                .filter(channel -> channel.getId().equals(channelId))
+//                .findFirst();
+//    }
+//
+//    // 채널 전체 조회
+//    @Override
+//    public List<ChannelEntity> findAll() {
+//        return data;
+//    }
+//
+//    // 채널 삭제
+//    @Override
+//    public void delete(ChannelEntity channel) {
+//        data.remove(channel);
+//    }
+//}
--- src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFMessageRepository.java
@@ -1,62 +1,62 @@
-package com.sprint.mission.discodeit.repository.jcf;
-
-import com.sprint.mission.discodeit.entity.MessageEntity;
-import com.sprint.mission.discodeit.entity.MessageEntity;
-import com.sprint.mission.discodeit.repository.MessageRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Repository;
-
-import java.time.Instant;
-import java.util.*;
-
-@Repository
-@ConditionalOnProperty(
-        name = "discodeit.repository.type" ,
-        havingValue = "jcf" ,
-        matchIfMissing = true
-)
-public class JCFMessageRepository implements MessageRepository {
-    private final List<MessageEntity> data;       // 모든 메시지
-
-    public JCFMessageRepository() {
-        data = new ArrayList<>();
-    }
-
-    // 메시지 저장
-    @Override
-    public void save(MessageEntity message) {
-        data.removeIf(existMessage -> existMessage.getId().equals(message.getId()));
-
-        data.add(message);
-    }
-
-    // 메시지 단건 조회
-    @Override
-    public Optional<MessageEntity> findById(UUID messageId) {
-        return data.stream()
-                .filter(message -> message.getId().equals(messageId))
-                .findFirst();
-    }
-
-    // 메시지 전체 조회
-    @Override
-    public List<MessageEntity> findAll() {
-        return data;
-    }
-
-    // 메시지 삭제
-    @Override
-    public void delete(MessageEntity message) {
-        data.remove(message);
-    }
-
-    @Override
-    public Instant getLastMessageAt(UUID channelId) {
-        return findAll().stream()
-                .filter(message -> message.getChannelId().equals(channelId))
-                .max(Comparator.comparing(MessageEntity::getCreatedAt))
-                .map(MessageEntity::getCreatedAt)
-                .orElse(null);
-    }
-
-}
+//package com.sprint.mission.discodeit.repository.jcf;
+//
+//import com.sprint.mission.discodeit.entity.MessageEntity;
+//import com.sprint.mission.discodeit.entity.MessageEntity;
+//import com.sprint.mission.discodeit.repository.MessageRepository;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.stereotype.Repository;
+//
+//import java.time.Instant;
+//import java.util.*;
+//
+//@Repository
+//@ConditionalOnProperty(
+//        name = "discodeit.repository.type" ,
+//        havingValue = "jcf" ,
+//        matchIfMissing = true
+//)
+//public class JCFMessageRepository implements MessageRepository {
+//    private final List<MessageEntity> data;       // 모든 메시지
+//
+//    public JCFMessageRepository() {
+//        data = new ArrayList<>();
+//    }
+//
+//    // 메시지 저장
+//    @Override
+//    public void save(MessageEntity message) {
+//        data.removeIf(existMessage -> existMessage.getId().equals(message.getId()));
+//
+//        data.add(message);
+//    }
+//
+//    // 메시지 단건 조회
+//    @Override
+//    public Optional<MessageEntity> findById(UUID messageId) {
+//        return data.stream()
+//                .filter(message -> message.getId().equals(messageId))
+//                .findFirst();
+//    }
+//
+//    // 메시지 전체 조회
+//    @Override
+//    public List<MessageEntity> findAll() {
+//        return data;
+//    }
+//
+//    // 메시지 삭제
+//    @Override
+//    public void delete(MessageEntity message) {
+//        data.remove(message);
+//    }
+//
+//    @Override
+//    public Instant getLastMessageAt(UUID channelId) {
+//        return findAll().stream()
+//                .filter(message -> message.getChannelId().equals(channelId))
+//                .max(Comparator.comparing(MessageEntity::getCreatedAt))
+//                .map(MessageEntity::getCreatedAt)
+//                .orElse(null);
+//    }
+//
+//}
