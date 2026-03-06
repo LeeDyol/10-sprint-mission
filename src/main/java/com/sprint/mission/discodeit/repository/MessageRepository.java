b1adf9b33bab0a6ed341495a255f0759691801ea
LeeDyol <dlekthf819203@naver.com>
2026-03-06 17:22:38 +0900

    fix: MessageRepository 내 채널별 마지막 메시지 시간 조회 쿼리 구현

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.Instant;
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, UUID> {
     // 특정 채널에서 마지막으로 발행된 메시지 시간 조회
+    @Query("SELECT MAX(m.createdAt) FROM MessageEntity m WHERE m.channel.id = :channelId")
     Instant getLastMessageAt(UUID channelId);
 }
