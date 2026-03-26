affb26fe2f2df8c4c072f6a88bad865002b31d65
LeeDyol <dlekthf819203@naver.com>
2026-03-26 19:48:19 +0900

    feat: 일관된 예외 응답을 위한 ErrorResponse 정의

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorResponse.java
@@ -3,12 +3,16 @@ package com.sprint.mission.discodeit.exception;
 import lombok.Builder;
 
 import java.time.Instant;
+import java.util.Map;
 
+// 에러 응답 DTO
 @Builder
 public record ErrorResponse (
-    Instant timestamp,
-    String code,
-    int status,
-    String message
+    Instant timestamp,                  // 에러 발생 시각
+    String code,                        // 발생한 에러 코드
+    String message,                     // 발생한 에러 메시지
+    Map<String, Object> details,        // 발생한 에러와 관련된 추가 정보
+    String exceptionType,               // 발생한 에러 클래스 이름
+    int status                          // HTTP 상태 코드
 ) {
 }
