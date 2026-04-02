4e4e3a22e6975e2cf4b656a5e380e88a29da3709
LeeDyol <dlekthf819203@naver.com>
2026-04-02 10:28:15 +0900

    test: User, Channel, Message 컨트롤러 단위 테스트 작성

---
+++ src/test/java/com/sprint/mission/discodeit/controller/ChannelControllerTest.java
@@ -0,0 +1,264 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.dto.request.channel.PrivateChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.channel.PublicChannelCreateRequest;
+import com.sprint.mission.discodeit.dto.request.channel.PublicChannelUpdateRequest;
+import com.sprint.mission.discodeit.dto.response.ChannelDto;
+import com.sprint.mission.discodeit.entity.ChannelType;
+import com.sprint.mission.discodeit.service.ChannelService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(ChannelController.class)
+public class ChannelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private ChannelService channelService;
+
+    /*
+        특정 사용자가 참여하고 있는 목록 조회
+     */
+    // [성공]
+    @Test
+    @DisplayName("특정 사용자가 참여하고 있는 목록 조회 완료")
+    void find_all_by_user_id_success() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        ChannelDto firstChannel = ChannelDto.builder()
+                .id(UUID.randomUUID())
+                .name("Alien")
+                .description("To be Alien together")
+                .build();
+        ChannelDto secondChannel = ChannelDto.builder()
+                .id(UUID.randomUUID())
+                .name("Meow")
+                .description("My cat is god")
+                .build();
+
+        List<ChannelDto> channels = List.of(firstChannel, secondChannel);
+        given(channelService.findAllByUserId(userId)).willReturn(channels);
+
+        // when & then
+        mockMvc.perform(get("/api/channels")
+                        .param("userId", userId.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Alien"));
+    }
+
+    // [실패] 파라미터 누락
+    @Test
+    @DisplayName("특정 사용자가 참여하고 있는 목록 조회 실패: 특정 사용자 ID가 누락된 경우, 400 Bad Request 반환")
+    void find_all_by_user_id_failure() throws Exception {
+        // given
+
+        // when & then
+        mockMvc.perform(get("/api/channels"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MissingServletRequestParameterException"));
+    }
+
+    /*
+        채널 생성
+     */
+    // [성공]
+    @Test
+    @DisplayName("공개 채널 생성 완료")
+    void create_public_channel_success() throws Exception {
+        // given
+        UUID channelId = UUID.randomUUID();
+        PublicChannelCreateRequest request = new PublicChannelCreateRequest(
+                "Alien",
+                "To be Alien together"
+        );
+
+        // 생성할 공개 채널
+        ChannelDto responseDto = ChannelDto.builder()
+                .id(channelId)
+                .type(ChannelType.PUBLIC)
+                .name(request.name())
+                .description(request.description())
+                .lastMessageAt(Instant.now())
+                .build();
+        given(channelService.createPublicChannel(request)).willReturn(responseDto);
+
+        // when & then
+        mockMvc.perform(post("/api/channels/public")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(channelId.toString()))
+                .andExpect(jsonPath("$.name").value("Alien"))
+                .andExpect(jsonPath("$.type").value("PUBLIC"));
+    }
+
+    // [실패] 채널명 공백
+    @Test
+    @DisplayName("공개 채널 생성 실패: 채널명이 누락될 경우, 400 Bad Request 반환")
+    void create_public_channel_failure() throws Exception {
+        // given
+        String json = """
+                {
+                    "description": "To be Alien together"
+                }
+                """;
+
+        // When & Then
+        mockMvc.perform(post("/api/channels/public")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MethodArgumentNotValidException"));
+    }
+
+    // [성공]
+    @Test
+    @DisplayName("비공개 채널 생성 완료")
+    void create_private_channel_success() throws Exception {
+        // given
+        UUID channelId = UUID.randomUUID();
+
+        // 비공개 채널 참여자 목록
+        List<UUID> participantIds = List.of(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
+        PrivateChannelCreateRequest request = new PrivateChannelCreateRequest(participantIds);
+
+        // 생성할 공개 채널
+        ChannelDto responseDto = ChannelDto.builder()
+                .id(channelId)
+                .type(ChannelType.PRIVATE)
+                .lastMessageAt(Instant.now())
+                .build();
+        given(channelService.createPrivateChannel(any(PrivateChannelCreateRequest.class))).willReturn(responseDto);
+
+
+        // when & then
+        mockMvc.perform(post("/api/channels/private")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(channelId.toString()));
+    }
+
+    // [실패] 참여자 목록 누락
+    @Test
+    @DisplayName("비공개 채널 생성 실패: 참여자 목록이 누락될 경우, 400 Bad Request 반환")
+    void create_private_channel_failure() throws Exception {
+        // given
+
+        // 비공개 채널 참여자
+        PrivateChannelCreateRequest request = new PrivateChannelCreateRequest(null);
+
+        // When & Then
+        mockMvc.perform(post("/api/channels/private")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MethodArgumentNotValidException"));
+    }
+
+    /*
+        공개 채널 수정
+     */
+    // [성공]
+    @Test
+    @DisplayName("공개 채널 수정 완료")
+    void update_public_channel_success() throws Exception {
+        // given
+        UUID channelId = UUID.randomUUID();
+        PublicChannelUpdateRequest request = new PublicChannelUpdateRequest(
+                "Meow",
+                "My cat is god"
+        );
+
+        // 수정할 채널 객체
+        ChannelDto response = ChannelDto.builder()
+                .id(channelId)
+                .type(ChannelType.PUBLIC)
+                .name(request.newName())
+                .description(request.newDescription())
+                .lastMessageAt(Instant.now())
+                .build();
+        given(channelService.update(eq(channelId), any(PublicChannelUpdateRequest.class))).willReturn(response);
+
+        // when & then
+        mockMvc.perform(patch("/api/channels/{channelId}", channelId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Meow"));
+    }
+
+    // [실패] 잘못된 채널 ID 전달
+    @Test
+    @DisplayName("공개 채널 수정 실패: 채널 ID가 잘못된 경우, 400 Bad Request 반환")
+    void update_public_channel_failure() throws Exception {
+        // given
+        String channelId = "channelId";
+        PublicChannelUpdateRequest request = new PublicChannelUpdateRequest(
+                "Meow",
+                "My cat is god"
+        );
+
+        // when & then
+        mockMvc.perform(patch("/api/channels/{channelId}", channelId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MethodArgumentTypeMismatchException"));
+    }
+
+    /*
+        채널 삭제
+     */
+    // [성공]
+    @Test
+    @DisplayName("채널 삭제 완료")
+    void delete_public_channel_success() throws Exception {
+        // given
+        UUID channelId = UUID.randomUUID();
+
+        // when & then
+        mockMvc.perform(delete("/api/channels/{channelId}", channelId))
+                .andExpect(status().isNoContent());
+    }
+
+    // [실패]류 HTTP 메서드 오류
+    @Test
+    @DisplayName("채널 삭제 실패: HTTP 메서드가 잘못된 경우, 405 Method Not Allowed 반환")
+    void delete_public_channel_failure() throws Exception {
+        // given
+        UUID channelId = UUID.randomUUID();
+
+        // when & then
+        mockMvc.perform(post("/api/channels/{channelId}", channelId))
+                .andExpect(status().isMethodNotAllowed())
+                .andExpect(jsonPath("$.exceptionType").value("HttpRequestMethodNotSupportedException"));
+    }
+}
---
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -0,0 +1,293 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.dto.request.user.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.user.UserUpdateRequest;
+import com.sprint.mission.discodeit.dto.request.userstatus.UserStatusUpdateRequest;
+import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.dto.response.UserStatusDto;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+
+    @MockitoBean
+    private UserStatusService userStatusService;
+
+    /*
+        전체 목록 조회
+     */
+    // [성공]
+    @Test
+    @DisplayName("전체 목록 조회 완료")
+    void find_all_user_success() throws Exception {
+        // given
+        UserDto firstUserDto = UserDto.builder().build();
+        UserDto secondUserDto = UserDto.builder().build();
+        List<UserDto> users = List.of(firstUserDto, secondUserDto);
+
+        given(userService.findAll()).willReturn(users);
+
+        // when & then
+        mockMvc.perform(get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    /*
+        사용자 등록
+     */
+    // [성공]
+    @Test
+    @DisplayName("사용자 등록 완료")
+    void create_user_success() throws Exception {
+        // given
+        UserCreateRequest request = new UserCreateRequest(
+                "yushi",
+                "yushi@wish.com",
+                "yushi1234"
+        );
+        MockMultipartFile profilePart = new MockMultipartFile(
+                "profile",
+                "test.png",
+                "image/png",
+                "image".getBytes()
+        );
+
+        // JSON 파일화
+        MockMultipartFile requestPart = new MockMultipartFile(
+                "userCreateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+
+        // 생성할 사용자
+        UserDto user = UserDto.builder()
+                .id(UUID.randomUUID())
+                .username(request.username())
+                .email(request.email())
+                .profile(null)
+                .build();
+
+        given(userService.create(any(), any())).willReturn(user);
+
+        // when & then
+        mockMvc.perform(multipart("/api/users")
+                        .file(requestPart)
+                        .file(profilePart)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(user.id().toString()));
+    }
+
+    // [실패] 이메일 형식 오류
+    @Test
+    @DisplayName("사용자 등록 실패: 잘못된 이메일 형식을 입력한 경우, 400 Bad Request 반환")
+    void create_user_failure() throws Exception {
+        // given
+        UserCreateRequest request = new UserCreateRequest(
+                "yushi",
+                "yushi",
+                "yushi1234"
+        );
+        MockMultipartFile profilePart = new MockMultipartFile(
+                "profile",
+                "test.png",
+                "image/png",
+                "image".getBytes()
+        );
+
+        // JSON 파일화
+        MockMultipartFile requestPart = new MockMultipartFile(
+                "userCreateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/api/users")
+                        .file(requestPart)
+                        .file(profilePart)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MethodArgumentNotValidException"));
+    }
+
+    /*
+        사용자 정보 수정
+     */
+    // [성공]
+    @Test
+    @DisplayName("사용자 정보 수정 완료")
+    void update_user_success() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+        UserUpdateRequest request = new UserUpdateRequest(
+                "tokuno",
+                "tokuno@wish.com",
+                "tokuno1234"
+        );
+
+        // JOSN
+        MockMultipartFile requestPart = new MockMultipartFile(
+                "userUpdateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+
+        // 수정할 사용자
+        UserDto user = UserDto.builder()
+                .id(userId)
+                .username(request.newUsername())
+                .email(request.newEmail())
+                .profile(null)
+                .build();
+        given(userService.update(eq(userId), any(), any())).willReturn(user);
+
+        // when & then
+        mockMvc.perform(multipart("/api/users/{userId}", userId)
+                        .file(requestPart)
+                        .with(req -> {
+                            req.setMethod("PATCH");
+                            return req;
+                        })
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(request.newUsername()));
+    }
+
+    // [실패] 잘못된 사용자 ID 전달
+    @Test
+    @DisplayName("사용자 정보 수정 실패: 잘못된 사용자 ID를 전달하 경우, 400 Bad Request 반환")
+    void update_user_failure() throws Exception {
+        // given
+        String userId = "userId";
+        UserUpdateRequest request = new UserUpdateRequest(
+                "tokuno",
+                "tokuno@wish.com",
+                "tokuno1234"
+        );
+
+        // JOSN
+        MockMultipartFile requestPart = new MockMultipartFile(
+                "userUpdateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsString(request).getBytes()
+        );
+
+        // when & then
+        mockMvc.perform(multipart("/api/users/{userId}", userId)
+                        .file(requestPart)
+                        .with(req -> {
+                            req.setMethod("PATCH");
+                            return req;
+                        })
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.exceptionType").value("MethodArgumentTypeMismatchException"));
+    }
+
+    /*
+        사용자 삭제
+     */
+    // [성공]
+    @Test
+    @DisplayName("사용자 삭제 완료")
+    void delete_user_success() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        // when & then
+        mockMvc.perform(delete("/api/users/{userId}", userId))
+                .andExpect(status().isNoContent());
+    }
+
+    // [실패] HTTP 메서드 오류
+    @Test
+    @DisplayName("사용자 삭제 실패: HTTP 메서드가 잘못된 경우, 400 Bad Request 반환")
+    void delete_user_failure() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+
+        // when & then
+        mockMvc.perform(post("/api/users/{userId}", userId))
+                .andExpect(status().isMethodNotAllowed())
+                .andExpect(jsonPath("$.exceptionType").value("HttpRequestMethodNotSupportedException"));
+    }
+
+    /*
+        사용자 온라인 상태 업데이트
+     */
+    // [성공]
+    @Test
+    @DisplayName("사용자 온라인 상태 업데이트 완료")
+    void update_user_status_success() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+        Instant lastActiveAt = Instant.now();
+        UserStatusUpdateRequest request = new UserStatusUpdateRequest(lastActiveAt);
+
+        // 업데이트할 온라인 상태
+        UserStatusDto userStatus = UserStatusDto.builder()
+                .id(UUID.randomUUID())
+                .userId(userId)
+                .lastActiveAt(request.newLastActiveAt())
+                .build();
+        given(userStatusService.updateByUserId(eq(userId), any())).willReturn(userStatus);
+
+        // when & then
+        mockMvc.perform(patch("/api/users/{userId}/userStatus", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(userStatus.userId().toString()));
+    }
+
+    // [실패]
+    @Test
+    @DisplayName("사용자 온라인 상태 업데이트 실패: ")
+    void update_user_status_failure() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+        UserStatusUpdateRequest request = new UserStatusUpdateRequest(null);
+
+        // when & then
+        mockMvc.perform(patch("/api/users/{userId}/userStatus", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+}
