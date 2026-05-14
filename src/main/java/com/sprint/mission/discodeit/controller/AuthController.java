d135ccf936d56a279a658590e1ec4c9922e0eb9c
LeeDyol <dlekthf819203@naver.com>
2026-05-14 15:47:34 +0900

    feat: 세션 기반 현재 사용자 정보 조회 API 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
-    @Operation(summary = "토큰 발급", operationId = "token")
+    @Operation(summary = "CSRF 토큰 발급", operationId = "token")
     @GetMapping("/csrf-token")
     public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
         String tokenValue = csrfToken.getToken();
@@ -24,5 +27,17 @@ public class AuthController {
         log.debug("CSRF 토큰 요청: {}", tokenValue);
 
         return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+    }
+
+    @Operation(summary = "현재 로그인 한 사용자 정보 조회", operationId = "getCurrentUser")
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> getAuthenticatedUser(@AuthenticationPrincipal DiscodeitUserDetails discodeitUserDetails) {
+
+        // 비로그인 사용자 요청 시, 401 에러 반환
+        if (discodeitUserDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(discodeitUserDetails.getUserDto());
     }
 }
