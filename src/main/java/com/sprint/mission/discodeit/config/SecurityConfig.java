999defafe1db5e96f6c05102aa3023ac679cfb7b
LeeDyol <dlekthf819203@naver.com>
2026-05-14 10:38:32 +0900

    feat: SPA 환경의 CSRF 방어를 위한 SpaCsrfTokenRequestHandler 구현 및 적용

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+/*
+    SecurityConfig
+    --------------
+    프로젝트 전체의 보안 통제 및 보안 필터 조
+ */
 @Configuration
 @EnableWebSecurity(debug = true)        // 필터 목록 콘솔에 출력
 public class SecurityConfig {
@@ -14,9 +19,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // HTTP 보안 설정
         http
+                // CSRF (크로스 사이트 요청 위조) 방어 설정
                 .csrf(csrf -> csrf
+                        // 토큰을 쿠키에 저장하되, 프론트가 읽을 수 있도록 HttpOnly 방어막 해제
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        // 토큰 검증 주제 설정
+                        .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
                 );
 
         return http.build();
---
+++ src/main/java/com/sprint/mission/discodeit/config/SpaCsrfTokenRequestHandler.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.util.StringUtils;
+
+import java.util.function.Supplier;
+
+/*
+    SpaCsrfTokenRequestHandler
+    --------------------------
+    기본 CSRF 검문소 대체
+ */
+public class SpaCsrfTokenRequestHandler implements CsrfTokenRequestHandler {
+    private final CsrfTokenRequestHandler plain = new CsrfTokenRequestAttributeHandler();           // 기본 토큰 처리
+    private final CsrfTokenRequestHandler xor = new XorCsrfTokenRequestAttributeHandler();          // 마스크(XOR) 토큰 처리
+
+    // 응답: 서버가 클리이언트에게 토큰 발급
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, Supplier<CsrfToken> csrfToken) {
+        // 마스크(XOR) 적용
+        this.xor.handle(request, response, csrfToken);
+        csrfToken.get();
+    }
+
+    // 요청: 클라이언트의 토큰 검증
+    @Override
+    public String resolveCsrfTokenValue(HttpServletRequest request, CsrfToken csrfToken) {
+        // 요청 헤더(X-XSRF-TOKEN)로부터 토큰 분리
+        String headerValue = request.getHeader(csrfToken.getHeaderName());
+
+        return (StringUtils.hasText(headerValue)
+                ? this.plain                                                // 헤더에 토큰이 있립 경우
+                : this.xor).resolveCsrfTokenValue(request, csrfToken);      // 헤더에 토긑니 없을 경우
+    }
+}
