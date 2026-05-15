c95f36aa75ee67a24ce7d9e80de022158e8d334e
LeeDyol <dlekthf819203@naver.com>
2026-05-15 17:03:38 +0900

    refactor: /me API 최신 데이터 조회 로직 적용 및 중복 검증 코드 제거

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.sprint.mission.discodeit.dto.request.auth.RoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -15,6 +16,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 /*
     TODO: Swagger API 명세서 세부 작업
  */
@@ -25,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
     @Operation(summary = "CSRF 토큰 발급", operationId = "token")
     @GetMapping("/csrf-token")
@@ -39,13 +43,11 @@ public class AuthController {
     @Operation(summary = "현재 로그인 한 사용자 정보 조회", operationId = "getCurrentUser")
     @GetMapping("/me")
     public ResponseEntity<UserDto> getAuthenticatedUser(@AuthenticationPrincipal DiscodeitUserDetails discodeitUserDetails) {
+        // 데이터베이스에서 사용자 정보 조회
+        UUID userId = discodeitUserDetails.getUserDto().id();
+        UserDto currentUser = userService.findById(userId);
 
-        // 비로그인 사용자 요청 시, 401 에러 반환
-        if (discodeitUserDetails == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        return ResponseEntity.ok(discodeitUserDetails.getUserDto());
+        return ResponseEntity.ok(currentUser);
     }
 
     @Operation(summary = "사용자 권한 수정", operationId = "updateRole")
