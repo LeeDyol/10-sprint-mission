eed605349b55521424d1af78c9f0c946dc621a63
LeeDyol <dlekthf819203@naver.com>
2026-05-26 16:36:28 +0900

    feat: 리프레시 토큰 로테이션(RTR)을 적용한 토큰 재발급 API 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,12 +1,16 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.request.auth.RoleUpdateRequest;
+import com.sprint.mission.discodeit.dto.response.auth.JwtDto;
 import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.dto.response.auth.TokenDto;
 import com.sprint.mission.discodeit.security.common.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import com.sprint.mission.discodeit.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,7 +34,7 @@ public class AuthController {
     private final AuthService authService;
     private final UserService userService;
 
-    @Operation(summary = "CSRF 토큰 발급", operationId = "token")
+    @Operation(summary = "CSRF 토큰 발급", operationId = "CSRFToken")
     @GetMapping("/csrf-token")
     public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
         String tokenValue = csrfToken.getToken();
@@ -38,6 +42,22 @@ public class AuthController {
         log.debug("CSRF 토큰 요청: {}", tokenValue);
 
         return ResponseEntity.status(HttpStatus.NON_AUTHORITATIVE_INFORMATION).build();
+    }
+
+    @Operation(summary = "AccessToken 재발급", operationId = "refreshToken")
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtDto> refreshToken(@CookieValue(value = "REFRESH_TOKEN", required = false) String refreshToken,
+                                               HttpServletResponse response) {
+        TokenDto tokenDto = authService.reissueRefreshToken(refreshToken);
+
+        // 리프레시 토큰을 저장할 쿠키 객체
+        Cookie refreshTokenCookie = new Cookie("REFRESH_TOKEN", refreshToken);
+        refreshTokenCookie.setHttpOnly(true);               // 자바스크립트 읽기 방지
+        refreshTokenCookie.setPath("/");                    // 모든 경로에서 사용
+        refreshTokenCookie.setMaxAge(60 * 60 * 24 * 14);    // 유효 기간 (14일)
+        response.addCookie(refreshTokenCookie);             // 응답 헤더 내 포함
+
+        return ResponseEntity.ok(tokenDto.jwtDto());
     }
 
     @Operation(summary = "현재 로그인 한 사용자 정보 조회", operationId = "getCurrentUser")
