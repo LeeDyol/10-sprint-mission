c6f125983a5180cec791c348304bcd74faf3d9dd
LeeDyol <dlekthf819203@naver.com>
2026-05-26 16:40:56 +0900

    feat: AuthService 내 토큰 재발급 로직 구현

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,19 +1,27 @@
 package com.sprint.mission.discodeit.service.basic;
 
 import com.sprint.mission.discodeit.dto.request.auth.RoleUpdateRequest;
+import com.sprint.mission.discodeit.dto.response.auth.JwtDto;
 import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.dto.response.auth.TokenDto;
 import com.sprint.mission.discodeit.entity.UserEntity;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import com.sprint.mission.discodeit.exception.auth.JwtTokenUnauthorizedException;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
+import com.sprint.mission.discodeit.mapper.AuthMapper;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.security.auth.jwt.JwtTokenProvider;
 import com.sprint.mission.discodeit.security.common.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.UUID;
 
@@ -26,6 +34,29 @@ public class BasicAuthService implements AuthService {
     private final SessionRegistry sessionRegistry;
 
     private final UserMapper userMapper;
+    private final AuthMapper authMapper;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    // refreshToken 재발급
+    @Override
+    public TokenDto reissueRefreshToken(String refreshToken) {
+        // JWT 토큰 검증
+        if (!StringUtils.hasText(refreshToken) || !jwtTokenProvider.validateToken(refreshToken)) {
+            throw new JwtTokenUnauthorizedException(ErrorCode.JWT_TOKEN_UNAUTHORIZED);
+        }
+
+        // 토큰 재발급
+        Authentication authentication = jwtTokenProvider.getAuthentication(refreshToken);
+        String newAccessToken = jwtTokenProvider.generateAccessToken(authentication);
+        String newRefreshToken = jwtTokenProvider.generateRefreshToken(authentication);
+
+        // 인증 정보 추출
+        DiscodeitUserDetails userDetails = (DiscodeitUserDetails) authentication.getPrincipal();
+        JwtDto jwtDto = authMapper.toJwtDto(newAccessToken, userDetails.getUserDto());
+
+        return authMapper.toTokenDto(jwtDto, newRefreshToken);
+    }
 
     // 사용자 권한 변경
     @Override
