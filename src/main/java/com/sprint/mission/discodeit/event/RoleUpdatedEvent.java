08e8e3e0dceb8d7467912abc42e0fc8cdb840bf6
LeeDyol <dlekthf819203@naver.com>
2026-06-02 18:41:17 +0900

    feat: RoleUpdatedEvent 정의 및 AuthService 내 권한 변경 알림 이벤트 발행

---
+++ src/main/java/com/sprint/mission/discodeit/event/RoleUpdatedEvent.java
@@ -0,0 +1,16 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.entity.Role;
+
+import java.util.UUID;
+
+/*
+    RoleUpdatedEvent
+    ----------------
+
+ */
+public record RoleUpdatedEvent(
+        UUID userId,
+        Role newRole
+) {
+}
